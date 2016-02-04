e9365353e53b6df19b35cfd2da07d191d1a67d0c
Algram <aliasgram@gmail.com>
2016-02-04 22:35:16 +0100

    Made autocomplete case-insensitive

--- app/js/util.js
+++ app/js/util.js
@@ -102,9 +102,10 @@ function autocomplete(str, users, callback) {
 	for (let key in users) {
 		let user = users[key];
 		user = user.split(':')[0];
+		user = user.toLowerCase();
 
 		//Check if str is the start of user
-		if (user.indexOf(str) === 0) {
+		if (user.indexOf(str.toLowerCase()) === 0) {
 			callback(user);
 		}
 	}
